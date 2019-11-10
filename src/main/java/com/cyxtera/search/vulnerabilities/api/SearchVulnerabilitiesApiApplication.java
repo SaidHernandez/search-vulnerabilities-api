35834e849d392f82b844e96dee3cf4d0ef782a7f
SaidHernandez <said.ingsistemas@gmail.com>
2019-11-10 09:00:46 -0500

    Base project

---
+++ src/main/java/com/cyxtera/search/vulnerabilities/api/SearchVulnerabilitiesApiApplication.java
@@ -0,0 +1,13 @@
+package com.cyxtera.search.vulnerabilities.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchVulnerabilitiesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchVulnerabilitiesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyxtera/search/vulnerabilities/api/SearchVulnerabilitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyxtera.search.vulnerabilities.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchVulnerabilitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
